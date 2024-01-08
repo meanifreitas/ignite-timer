d68b997ee38ee4fac81f32301879d760d1f5527e
Meani Freitas <meani.sf@gmail.com>
2024-01-08 11:10:58 -0300

    Form validation and dynamic values for CountDown component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,9 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import React from 'react'
+
 import {
   CountDownContainer,
   FormContainer,
@@ -9,16 +14,71 @@ import {
   TimeInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Fill out the project name'),
+  time: zod.number().min(5).max(60),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+  id: string
+  task: string
+  time: number
+}
+
 export function Home() {
+  const [cycles, setCycles] = React.useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = React.useState<string | null>(null)
+  const [secondsPassed, setSecondsPassed] = React.useState(0)
+
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      time: 0,
+    },
+  })
+
+  function handleCreateNewCicle(data: NewCycleFormData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      time: data.time,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+
+    reset()
+  }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const totalSeconds = activeCycle ? activeCycle.time * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - secondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCicle)}>
         <FormContainer>
           <label htmlFor="task">I will be working on</label>
           <TaskInput
             list="task-suggestions"
             id="task"
             placeholder="Give your project a name"
+            {...register('task')}
           />
           <datalist id="task-suggestions">
             <option value="Project 1" />
@@ -34,20 +94,21 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('time', { valueAsNumber: true })}
           />
 
           <span>minutes.</span>
         </FormContainer>
 
         <CountDownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountDownContainer>
 
-        <StartButton type="submit">
+        <StartButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Start
         </StartButton>
