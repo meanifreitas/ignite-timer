3e51c6c2672f1868a1e8e3eb4a6945a6fbe71e80
Meani Freitas <meani.sf@gmail.com>
2024-01-07 21:52:46 -0300

    Finishing Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,9 @@ import {
   FormContainer,
   HomeContainer,
   Separator,
+  StartButton,
+  TaskInput,
+  TimeInput,
 } from './styles'
 
 export function Home() {
@@ -12,10 +15,26 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">I will be working on</label>
-          <input id="task" />
+          <TaskInput
+            list="task-suggestions"
+            id="task"
+            placeholder="Give your project a name"
+          />
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+          </datalist>
 
           <label htmlFor="time">during</label>
-          <input type="number" id="time" />
+          <TimeInput
+            type="number"
+            id="time"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutes.</span>
         </FormContainer>
@@ -28,10 +47,10 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <button type="submit">
+        <StartButton type="submit">
           <Play size={24} />
           Start
-        </button>
+        </StartButton>
       </form>
     </HomeContainer>
   )
